f6ee848a3d64d9aad4f96062973fc2fb9385bba6
Sergey Demchuk <sergiusd@megaplan.ru>
2017-09-27 16:47:12 +0300

    Constants replacer

--- tests/stubs/converter/it-can-replace-null-coalesce-operators/php5.php
+++ tests/stubs/converter/it-can-replace-null-coalesce-operators/php5.php
@@ -10,12 +10,17 @@ function test2()
 }
 class Test
 {
+    const TEST_SCALAR = 3;
+    const TEST_ARRAY = array("t" => 666);
     public function testMethod()
     {
         return null;
     }
 }
 $test = new Test();
+$result = defined('Test::TEST_SCALAR') ? Test::TEST_SCALAR : 0;
+$result = (defined('Test::TEST_ARRAY') and !empty(Test::TEST_ARRAY["f"])) ? Test::TEST_ARRAY["f"] : 1;
+$result = (defined('Test::TEST_ARRAY') and !empty(Test::TEST_ARRAY["f"]["d"]["y"])) ? Test::TEST_ARRAY["f"]["d"]["y"] : 1;
 $result = isset($input) ? $input : 'fixed-value';
 $result = isset($input) ? $input : (isset($input2) ? $input2 : $input3);
 $result = !empty(test1()) ? test1() : (!empty(test2()) ? test2() : 0);
